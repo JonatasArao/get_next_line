4970bd24d72c74ea9b9a2e3b29a6be79a6b648f9
Jonatas Arão de Carvalho Neves <51247074+JonatasArao@users.noreply.github.com>
2024-11-04 15:34:58 -0300

    ft_strlen added

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,15 @@
 /*   By: jarao-de <jarao-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 08:36:37 by jarao-de          #+#    #+#             */
-/*   Updated: 2024/11/04 08:37:44 by jarao-de         ###   ########.fr       */
+/*   Updated: 2024/11/04 15:13:50 by jarao-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# include <stdlib.h>
+
+size_t	ft_strlen(const char *s);
 
 char	*get_next_line(int fd);
 
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,18 @@
 /*   By: jarao-de <jarao-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 08:36:39 by jarao-de          #+#    #+#             */
-/*   Updated: 2024/11/04 08:36:40 by jarao-de         ###   ########.fr       */
+/*   Updated: 2024/11/04 14:46:05 by jarao-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	length;
+
+	length = 0;
+	while (s[length])
+		length++;
+	return (length);
+}
